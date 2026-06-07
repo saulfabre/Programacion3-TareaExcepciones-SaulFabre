d1f6dc65903dd9d916442c58050e1f829822defe
saulfabre <saulfabre2@gmail.com>
2026-06-07 15:28:38 -0400

    Agregado metodo mostrarMenu a clase Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Menu {
+
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String args[]) {
 
+    }
+
+    public static int mostrarMenu() {
+
+        int opcion;
+
+        System.out.println("=========================");
+        System.out.println("REGISTRO DE USUARIOS");
+        System.out.println("=========================\n");
+
+        System.out.println("1. Registrar usuario ");
+        System.out.println("Salir \n");
+
+        System.out.print("Opcion: ");
+
+        opcion = scanner.nextInt();
+
+        return opcion;
     }
 }
